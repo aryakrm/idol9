897a20a02cfe0d1157feefb4005502572486ea1d
Arya Karimi <devaryakrm@gmail.com>
2023-03-06 11:16:59 +0300

    code updated

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -3,6 +3,7 @@ import "./AboutUs.scss";
 import { motion } from "framer-motion";
 // import { t } from "i18next";
 import { useTranslation } from "react-i18next";
+import Counts from "../counts/Counts";
 
 function AboutUs() {
   const { t } = useTranslation();
@@ -17,6 +18,26 @@ function AboutUs() {
         <h1>{t("who_are_we")}</h1>
         <p>Idol9 {t("about_desc")}</p>
         <img className="logo" src="logo.png" alt="Idol9 Logo" />
+        <div className="counts">
+          <div className="count">
+            <p>Projects Done</p>
+            <h2>
+              +<Counts n={540} />
+            </h2>
+          </div>
+          <div className="count">
+            <p>Projects in process</p>
+            <h2>
+              +<Counts n={12} />
+            </h2>
+          </div>
+          <div className="count">
+            <p>Companies Worked</p>
+            <h2>
+              +<Counts n={36} />
+            </h2>
+          </div>
+        </div>
       </motion.div>
       <motion.div
         initial={{ x: "100vh" }}
---
+++ src/components/counts/Counts.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+
+function Counts({ n }) {
+  const { number } = useSpring({
+    from: { number: 0 },
+    number: n,
+    delay: 1000,
+    config: { mass: 1, tension: 20, friction: 10 },
+  });
+  return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
+}
+
+export default Counts;
