0ce669b9a8c200489e808399b06e33f9168862dc
Arya Karimi <devaryakrm@gmail.com>
2023-03-20 13:56:24 +0300

    code updated

--- src/store/initImages.js
+++ src/store/initImages.js
@@ -3,6 +3,63 @@ import { nanoid } from "nanoid";
 const imageData = [
   {
     id: nanoid(),
+    src: "/assets/01.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/02.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/02.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/03.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/04.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/05.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/06.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/07.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/08.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/09.jpg",
+    alt: `image ${nanoid()}`,
+  },
+  {
+    id: nanoid(),
+    src: "/assets/010.jpg",
+    alt: `image ${nanoid()}`,
+  },
+
+  //--------------------------------------------------------------------------------------------------------
+  {
+    id: nanoid(),
     src: "/assets/extt1.jpg",
     alt: `image ${nanoid()}`,
   },
