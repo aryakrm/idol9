809210edf5f7cd07f3d08609b6e032554c62e2e3
Arya Karimi <devaryakrm@gmail.com>
2023-04-06 15:40:42 +0300

    icon added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import "./Footer.scss";
 import { ImInstagram } from "react-icons/im";
 import { ImLinkedin } from "react-icons/im";
 import { FaWhatsappSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -26,6 +27,12 @@ function Footer() {
           </a>
           <a target="_blank" href="https://wa.me/905550644012">
             <FaWhatsappSquare />
+          </a>
+          <a
+            target="_blank"
+            href="https://www.facebook.com/profile.php?id=100091068677033"
+          >
+            <FaFacebookSquare />
           </a>
         </div>
       </div>
